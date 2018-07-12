c1fbf90dd7270a2dbac9813ee96a8a2910e11d83
parkhs2018 <parkhs2018@gmail.com>
2018-07-12 17:38:03 +0900

    html coding complete css customizeing

--- js/common.js
+++ js/common.js
@@ -1,1 +1,7 @@
-document
+// 서브 네비게이션 숨김 잠금
+
+  $(document).ready(function sub_nav_show(){
+$( ".main_nav a" ).on( "click", function() {
+  $('.sub_nav').toggleClass("show");
+  console.log("click a");
+});}); 
